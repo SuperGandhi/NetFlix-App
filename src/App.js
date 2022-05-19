a185ee6d418200f882e9bab80c1583c18e68639c
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-19 17:20:35 -0500

    adding styles main and function truncateString to show description limit movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import {Routes, Route} from 'react-router-dom'
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </>
   )
 }
 
---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = '6a6b3fea00a89d26ef748078f5995421'
+
+const requests ={
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRanted: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/movie/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
