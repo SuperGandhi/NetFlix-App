cb565ebdb0b7ae0d963939b66d8f097370b2a8b5
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-22 23:50:33 -0500

    event onClick slide row

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -12,18 +12,28 @@ const Row = ({title, fetchURL}) => {
         })
     }, [fetchURL])
 
-    console.log(movies)
+    const slideLeft = ()=>{
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500;
+    };
+
+    const slideRight = ()=>{
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500;
+    };
+
+
     return (
     <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
         <div className='relative flex items-center group'>
-            <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'size={40}/>
+            <MdChevronLeft onClick={slideLeft}className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'size={40}/>
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                 {movies.map((item,id)=>(
                     <Movie key={id} item={item}/>
                 ))}
             </div>
-            <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'size={40}/>
+            <MdChevronRight onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'size={40}/>
         </div>
     </>
   )
