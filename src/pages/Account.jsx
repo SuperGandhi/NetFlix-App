387fec83db850d78e4c402db6d54007d08f463d0
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-24 14:34:38 -0500

    styles

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <>
+      <div className='w-full text-white'>
+      <img className='w-full h-[400px] object-cover'src="https://assets.nflxext.com/ffe/siteui/vlv3/6e32b96a-d4be-4e44-a19b-1bd2d2279b51/7a4bfd8d-ab2f-4b1c-bbdf-c59ff2a36db0/PE-es-20220516-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+      <div className='absolute top-[20%] p-4 md:p-8'>
+      <h1 className='text-3xl md:text-5xl font-bold'>My shows</h1>
+      </div>
+      </div>
+    </>
   )
 }
 
